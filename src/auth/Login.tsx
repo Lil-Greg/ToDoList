539857a6bb3ee36c814e5db57d26f05d06de67e8
Lil-Greg <gregorywilliams1289@gmail.com>
2025-02-05 23:41:41 -0500

    Added Login Show Password (took about 5 min, need to spend more time coding bc it's fun :) )

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -1,9 +1,34 @@
-import { Container } from "react-bootstrap";
+import { useState } from "react";
+import { Container, InputGroup } from "react-bootstrap";
+import { Form as FormBootstrap } from 'react-bootstrap';
+import { FaRegEye } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa";
+import { Form } from "react-router-dom";
+
 
 export default function Login() {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleShowPassword = () => {
+        setShowPassword(!showPassword);
+    }
     return (<>
         <Container className="simple-container">
             <h1>Login</h1>
+            <Form>
+                <FormBootstrap.FloatingLabel label="Username">
+                    <FormBootstrap.Control placeholder="Username" />
+                </FormBootstrap.FloatingLabel>
+
+                <InputGroup>
+                    <FormBootstrap.FloatingLabel label="Password">
+                        <FormBootstrap.Control placeholder="Password" />
+                    </FormBootstrap.FloatingLabel>
+                    {showPassword ? <FaRegEye onClick={handleShowPassword} /> : <FaRegEyeSlash onClick={handleShowPassword} />}
+                </InputGroup>
+
+                <a href="/forgot-password">Forgot Password</a>
+            </Form>
         </Container>
     </>)
 }
